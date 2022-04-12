361872645afcc3a2c5d7f4772a891eb119bae25e
nyaribari <anyaribari@gmail.com>
2022-04-12 23:39:56 +0300

    readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+# SKAE-HUB BOOTCAMP 
+
+Day 1 assignments for the skaehub Cycle-3-Bootcamp.
--- duplicates.py
+++ duplicates.py
@@ -5,6 +5,6 @@ Python program to remove duplicates from a list
 def drop_duplicates(my_list):
     return list(set(my_list))
 
-#invoke function
+#invoke function 
 skae_list = ['me','you','we','you','them','them']
 print(drop_duplicates(skae_list))
